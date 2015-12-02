cea07d248e190254334f029a66f257cf023a9b58
Ernad Husremovic <hernad@bring.out.ba>
2015-12-02 07:04:01 -0500

    iptables to docker container

--- scripts/init_netflix_proxy.sh
+++ scripts/init_netflix_proxy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-iptables -t nat -A PREROUTING -p udp -i eth0 --dport 53 -j DNAT --to-destination 127.0.0.1:5300
-iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 80 -j DNAT --to-destination 127.0.0.1:8080
-iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 443 -j DNAT --to-destination 127.0.0.1:4430
-
 if [[ ! -f /etc/iptables.rules ]] ; then
   iptables-save > /etc/iptables.rules
   echo created /etc/iptables.rules
@@ -12,7 +8,20 @@ fi
 echo build docker images
 ./build.sh -b 1
 
-echo echo "crontabl -e"
+
+
+BIND_CONTAINER_IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' bind`
+iptables -t nat -I PREROUTING -p udp -i eth0 --dport 53 -j DNAT --to-destination $BIND_CONTAINER_IP:53
+
+SNIPROXY_CONTAINER_IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' sniproxy`
+iptables -t nat -I PREROUTING -s adsl.out.ba/32 -i eth0 -p tcp --dport 80  -j DNAT --to-dest $SNIPROXY_CONTAINER_IP:80
+iptables -t nat -I PREROUTING -s adsl.out.ba/32 -i eth0 -p tcp --dport 443 -j DNAT --to-dest $SNIPROXY_CONTAINER_IP:443
+
+iptables-save > /etc/iptables.rules
+
+echo /etc/iptables.rules koristi update_firewall.pl skripta
+
+echo echo "crontab -e"
 echo echo "add next line:"
 echo "*/7 * * * * /opt/netflix-proxy/scripts/update_firewall.pl"
 
