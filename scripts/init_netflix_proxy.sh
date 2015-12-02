ac73fc2e1f670fa2e552850ade63a8e3f2ebe81b
Ernad Husremovic <hernad@bring.out.ba>
2015-12-02 09:29:30 -0500

    update_firewall.pl on init

--- scripts/init_netflix_proxy.sh
+++ scripts/init_netflix_proxy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+ROOT_DIR=/opt/netflix-proxy
+
+PATH=/sbin:/bin:/usr/bin:/usr/local/bin
+
 if [[ ! -f /etc/iptables.rules ]] ; then
   iptables-save > /etc/iptables.rules
   echo created /etc/iptables.rules
 fi
 
 echo build docker images
-cd /opt/netflix-proxy
-./build.sh -b 1
+cd $ROOT_DIR
+$ROOT_DIR/build.sh -b 1
 
 
 sleep 5
@@ -19,10 +23,12 @@ SNIPROXY_CONTAINER_IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}
 iptables -t nat -I PREROUTING -s adsl.out.ba/32 -i eth0 -p tcp --dport 80  -j DNAT --to-dest $SNIPROXY_CONTAINER_IP:80
 iptables -t nat -I PREROUTING -s adsl.out.ba/32 -i eth0 -p tcp --dport 443 -j DNAT --to-dest $SNIPROXY_CONTAINER_IP:443
 
-iptables -F FRIENDS
-iptables-save > /etc/iptables.rules
-
 echo /etc/iptables.rules koristi update_firewall.pl skripta
+/sbin/iptables -F FRIENDS
+/sbin/iptables-save > /etc/iptables.rules
+
+/opt/netflix-proxy/scripts/update_firewall.pl
+
 
 echo echo "crontab -e"
 echo echo "add next line:"
